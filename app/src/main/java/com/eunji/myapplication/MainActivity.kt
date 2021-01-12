1135b8d031be10e2c46a4c54a357d7967abb731c
Eunji <zluoy@naver.com>
2021-01-12 15:14:00 +0900

    bmi 계산기 완료 - 최종 계산값 앱 재시작시 자동으로 기록되도록 수정.

--- app/src/main/java/com/eunji/myapplication/MainActivity.kt
+++ app/src/main/java/com/eunji/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.eunji.myapplication
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,15 +12,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadData()
+
         button.setOnClickListener {
             textView.text ="버튼 눌렀어용"
         }
 
         button2.setOnClickListener {
+            saveData(height.text.toString().toInt(),
+                    weight.text.toString().toInt())
+
             val intent = Intent(this, ResultActivity::class.java)
             intent.putExtra("weight",weight.text.toString())
             intent.putExtra("height",height.text.toString())
             startActivity(intent)
+        }
+    }
+
+    private fun saveData(height: Int, weight: Int) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+
+        editor.putInt("KEY_HEIGHT", height)
+            .putInt("KEY_WEIGHT", weight)
+            .apply()
+
+    }
+
+    private fun loadData() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val height_val = pref.getInt("KEY_HEIGHT", 0)
+        val weight_val = pref.getInt("KEY_WEIGHT",0)
+
+        if(height_val != 0 && weight_val != 0) {
+            height.setText(height_val.toString())
+            weight.setText(weight_val.toString())
         }
     }
 }
